3217e8d5dc7108ac364e84f7f23bdcebd52199e6
Ak347 <amitkalyan47@gmail.com>
2022-02-11 14:24:55 +0530

    This is my first commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,9 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class DemoApplication {
-
+	// This is a main method
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
